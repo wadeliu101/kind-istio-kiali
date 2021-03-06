3b2fd771fe9e7653e8e3fc6e13d58d02deaf9721
劉品均 <grassshrimp@liupinjunde-MacBook-Pro.local>
2021-03-07 01:55:21 +0800

    new example bookinfo

---
+++ examples/bookinfo/main.tf
@@ -0,0 +1,37 @@
+resource "null_resource" "bookinfo" {
+  provisioner "local-exec" {
+    command = "kubectl label namespace default istio-injection=enabled"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl label namespace default istio-injection-"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/platform/kube/bookinfo.yaml"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/platform/kube/bookinfo.yaml"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/networking/bookinfo-gateway.yaml"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/networking/bookinfo-gateway.yaml"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/networking/destination-rule-all.yaml"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/networking/destination-rule-all.yaml"
+  }
+}
